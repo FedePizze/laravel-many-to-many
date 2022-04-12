41cea39392ebbb15850320ca38264a1c1351b9e6
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-04-12 17:28:44 +0200

    first commit

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['Tiësto', 'Purple Disco Machine', 'Don Diablo', 'Alok', 'Daft Punk', 'Martin Solveig', 'R3hab'];
+
+        foreach($tags as $tag) {
+
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($tag);
+            $new_tag->save();
+
+        }
+    }
+}
